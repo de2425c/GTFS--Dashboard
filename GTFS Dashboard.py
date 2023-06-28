1d192fad2e0f0fc038019783c3c6e2b47aa2cadc
ZzMinn <70859911+ZzMinn@users.noreply.github.com>
2023-06-28 12:02:19 -0400

    Add files via upload

---
+++ GTFS Dashboard.py
@@ -0,0 +1,443 @@
+import os
+os.chdir('/Users/zzmin/Desktop/GTFS')
+from google.transit import gtfs_realtime_pb2
+from datetime import datetime
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as go
+import geopandas as gpd
+from shapely.geometry import LineString
+import pandas as pd
+import dash_bootstrap_components as dbc
+from shapely.geometry import Point
+import osmnx as ox
+import geopandas as gpd
+import json
+import requests
+import urllib.request
+
+subfile = ['bus_bronx','bus_brooklyn','bus_manhattan','bus_queens','bus_staten_island','subway']
+
+dataframes = {} 
+
+for subdir in subfile:
+    folder_path = os.path.join(os.getcwd(), subdir)
+
+    agency = pd.read_csv(os.path.join(folder_path, 'agency.txt'))
+    calendar_dates = pd.read_csv(os.path.join(folder_path, 'calendar_dates.txt'))
+    calendar = pd.read_csv(os.path.join(folder_path, 'calendar.txt'))
+    routes = pd.read_csv(os.path.join(folder_path, 'routes.txt'))
+    shapes = pd.read_csv(os.path.join(folder_path, 'shapes.txt'))
+    stop_times = pd.read_csv(os.path.join(folder_path, 'stop_times.txt'))
+    stops = pd.read_csv(os.path.join(folder_path, 'stops.txt'))
+    trips = pd.read_csv(os.path.join(folder_path, 'trips.txt'))
+#     transfers = pd.read_csv(os.path.join(folder_path, 'transfers.txt'))
+
+    df = trips[['route_id', 'service_id', 'trip_id']]
+    df = df.merge(stop_times[['trip_id', 'arrival_time', 'departure_time', 'stop_sequence', 'stop_id']],
+                  left_on='trip_id', right_on='trip_id', how='left')
+    df = df.merge(stops[['stop_id', 'stop_name', 'stop_lat', 'stop_lon']],
+                  left_on='stop_id', right_on='stop_id', how='left')
+    df = df.merge(routes[['route_id', 'route_long_name', 'route_color']],
+                  left_on='route_id', right_on='route_id', how='left')
+
+    route_color_mapping = df.set_index('route_id')['route_color'].fillna('000000').astype(str).apply(lambda x: "#" + x).to_dict()
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['stop_lon'], df['stop_lat']))
+    gdf.crs = "EPSG:4326"
+    gdf['color'] = gdf['route_id'].map(route_color_mapping)
+    dataframes[subdir] = gdf
+
+transportation = ['Bus', 'Subway', 'Citibike']
+boroughs = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten_Island"]
+citibike_regions = ['NYC District', 'JC District', 'Hoboken District']
+subway_id = dataframes['subway']['route_id'].unique()
+bus_bronx_id = dataframes['bus_bronx']['route_id'].unique()
+bus_brooklyn_id = dataframes['bus_brooklyn']['route_id'].unique()
+bus_manhattan_id = dataframes['bus_manhattan']['route_id'].unique()
+bus_queens_id = dataframes['bus_queens']['route_id'].unique()
+bus_staten_island_id = dataframes['bus_staten_island']['route_id'].unique()
+
+def citibike_station_data():
+    station_info_url = "https://gbfs.citibikenyc.com/gbfs/en/station_information.json"
+    info_response = urllib.request.urlopen(station_info_url)
+    data_info = json.load(info_response)
+
+    station_status_url = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json'
+    status_response = urllib.request.urlopen(station_status_url)
+    data_status = json.load(status_response)
+
+    system_regions_url = 'https://gbfs.citibikenyc.com/gbfs/en/system_regions.json'
+    regions_response = urllib.request.urlopen(system_regions_url)
+    data_regions = json.load(regions_response)
+
+    info_df = pd.DataFrame(data_info['data']['stations']).set_index('station_id')
+    info_df = info_df[['name', 'lat', 'lon', 'capacity', 'legacy_id', 'region_id']]
+
+    status_df = pd.DataFrame(data_status['data']['stations']).set_index('station_id')
+    status_df = status_df[['num_docks_available', 'num_bikes_disabled', 'num_ebikes_available', 'num_bikes_available', 'num_docks_disabled', 'station_status', 'is_renting', 'is_returning', 'last_reported', 'is_installed']]
+
+    regions_df = pd.DataFrame(data_regions['data']['regions'])
+    regions_df.rename(columns={'name': 'region_name'}, inplace=True)
+
+    citibike_df = info_df.merge(status_df, left_index=True, right_index=True)
+    citibike_df = citibike_df.merge(regions_df[['region_id', 'region_name']], left_on='region_id', right_on='region_id')
+    citibike_gdf = gpd.GeoDataFrame(citibike_df,
+                                    geometry=gpd.points_from_xy(citibike_df['lon'], citibike_df['lat']),
+                                    crs="EPSG:4326")
+    return citibike_gdf
+
+with open("/Users/zzmin/Desktop/GTFS/subway_API_Key.txt", "r") as f:
+    subway_API_KEY = f.read().strip()
+
+with open("/Users/zzmin/Desktop/GTFS/bus_API_Key.txt", "r") as f:
+    bus_API_KEY = f.read().strip()
+       
+def export_subway_schedule(api_key):
+    urls = [
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g',
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz',
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l',
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-si'
+    ]
+
+    feed = gtfs_realtime_pb2.FeedMessage()
+
+    for url in urls:
+        response = requests.get(url, headers={'x-api-key': api_key})
+        feed_message = gtfs_realtime_pb2.FeedMessage()
+        feed_message.ParseFromString(response.content)
+        feed.entity.extend(feed_message.entity)
+
+    subway_schedule = []
+
+    for entity in feed.entity:
+        if entity.HasField('trip_update'):
+            trip_update = entity.trip_update
+            for stop_time_update in trip_update.stop_time_update:
+                arrival_time = datetime.fromtimestamp(stop_time_update.arrival.time).strftime("%Y-%m-%d %H:%M:%S")
+                departure_time = datetime.fromtimestamp(stop_time_update.departure.time).strftime("%Y-%m-%d %H:%M:%S")
+                stop_id = stop_time_update.stop_id
+                subway_schedule.append({
+                    'route': trip_update.trip.route_id,
+                    'arrival_time': arrival_time,
+                    'departure_time': departure_time,
+                    'stop_id': stop_id
+                })
+
+    return subway_schedule
+
+subway_schedule = export_subway_schedule(subway_API_KEY)
+
+def export_bus_schedule(api_key):
+    base_url = 'http://gtfsrt.prod.obanyc.com/tripUpdates'
+    request_url = f'{base_url}?key={api_key}'
+
+    response = requests.get(request_url)
+    data = response.content
+
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(data)
+
+    bus_schedule = []
+    
+    for entity in feed.entity:
+        if entity.HasField('trip_update'):
+            trip_update = entity.trip_update
+            route_id = trip_update.trip.route_id
+            for stop_time_update in trip_update.stop_time_update:
+                arrival_time = datetime.fromtimestamp(stop_time_update.arrival.time).strftime("%Y-%m-%d %H:%M:%S")
+                departure_time = datetime.fromtimestamp(stop_time_update.departure.time).strftime("%Y-%m-%d %H:%M:%S")
+                stop_id = stop_time_update.stop_id
+                bus_schedule.append({
+                    'route': route_id,
+                    'arrival_time': arrival_time,
+                    'departure_time': departure_time,
+                    'stop_id': stop_id
+                })
+
+    return bus_schedule
+
+bus_schedule = export_bus_schedule(bus_API_KEY)
+
+def export_bus_location(api_key):
+    base_url = "http://gtfsrt.prod.obanyc.com/vehiclePositions"
+    request_url = f'{base_url}?key={api_key}'
+
+    response = requests.get(request_url)
+    data = response.content
+
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(data)
+
+    location = []
+    
+    for entity in feed.entity:
+        if entity.HasField('vehicle'):
+            vehicle = entity.vehicle
+            vehicle_id = vehicle.vehicle.id
+            route_id = vehicle.trip.route_id if vehicle.trip.HasField('route_id') else ""
+            direction_id = vehicle.trip.direction_id if vehicle.trip.HasField('direction_id') else None
+            latitude = vehicle.position.latitude
+            longitude = vehicle.position.longitude
+            location.append({
+                'Vehicle ID': vehicle_id,
+                'Route ID': route_id,
+                'Direction ID': direction_id,
+                'Latitude': latitude,
+                'Longitude': longitude
+            })
+
+    return location
+
+bus_location = export_bus_location(bus_API_KEY)
+
+osm = ox.graph_from_place("New York City, United States", network_type="drive", simplify=False)
+nodes, osm = ox.graph_to_gdfs(osm)
+osm = osm.reset_index()[["osmid", "name", "geometry"]]
+
+styles = {'background': '#262729', 'textColor': '#ffffff', 'marginColor': '#0e1012'}
+
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    children=[
+        html.Br(),
+        html.H1(children='TRAFFIC DATA DASHBOARD', style={'textAlign': 'center'}),
+        html.Br(),
+        html.Table([
+            html.Tr([
+                html.Td([
+                    html.P("Transportation:", style={"font-weight": "bold", "font-size": "20px"}),
+                    dcc.Checklist(
+                        id='transport-selector',
+                        options=[{'label': method, 'value': method} for method in transportation],
+                        value=[transportation[1]],
+                        className='form-check',
+                        inputClassName='form-check-input',
+                        labelClassName='form-check-label')
+                ], style={'vertical-align': 'top', 'padding': '10px'}),  # First row, first column for transportation
+
+                html.Td([
+                    html.P("Bus Region:", style={"font-weight": "bold", "font-size": "20px"}),
+                    dcc.Checklist(
+                        id='boroughs_chosen',
+                        options=[{'label': str(b), 'value': b} for b in boroughs],
+                        value=[],
+                        className='form-check',
+                        inputClassName='form-check-input',
+                        labelClassName='form-check-label')
+                ], style={'vertical-align': 'top', 'padding': '10px'}),  # First row, second column for bus region
+
+                html.Td([
+                    html.P("Bus Route:", style={"font-weight": "bold", "font-size": "20px"}),
+                    dcc.Dropdown(
+                        id='bus-routes-dropdown',
+                        multi=True,
+                        clearable=False,
+                        options=[{'label': str(route_id), 'value': route_id} for route_id in subway_id],
+                        value=[subway_id[0]],
+                        style={'display': 'block', 'color': '#000000'}
+                    ),
+                    html.P("Subway Route:", style={"font-weight": "bold", "font-size": "20px"}),
+                    dcc.Dropdown(
+                        id='route-selector',
+                        options=[{'label': str(route_id), 'value': route_id} for route_id in subway_id],
+                        multi=True,
+                        value=[subway_id[0]],
+                        style={'display': 'block', 'color': '#000000'}
+                    ),
+                    html.P("Citibike Region:", style={"font-weight": "bold", "font-size": "20px"}),
+                    dcc.Dropdown(
+                        id='citibike-region-dropdown',
+                        options=[{'label': region, 'value': region} for region in citibike_regions],
+                        multi=True,
+                        style={'display': 'block', 'color': '#000000'}
+                    )
+                ], style={'vertical-align': 'top', 'padding': '5px'}),  # First row, third column for bus route, subway route, and Citibike region
+            ]),  # First row
+        ], style={'width': '80%', 'margin': 'auto'}), 
+
+        dbc.Card([
+            dbc.CardBody([
+                dcc.Graph(id='map',style={'width': '100%', 'height': '100%'}),
+                html.Div(id='info-box', style={'display': 'none'}),
+                html.Div(id='info-content'),
+                html.Div(id='real-time-data', style={'display': 'none'}),  # Container for real-time data
+                dcc.Interval(
+                    id='interval-component',
+                    interval=60 * 1000,  # in milliseconds
+                    n_intervals=0
+                )
+            ])
+        ], style={"margin": "10px", "background-color": styles['background'], "color": styles['textColor']}),  
+    ],
+    style={"margin": "0", "background-color": styles['background'], "color": styles['textColor'], "border-color": styles['background']}
+)
+
+@app.callback(
+    [Output('bus-routes-dropdown', 'style'),
+     Output('route-selector', 'style'),
+     Output('citibike-region-dropdown', 'style')],
+    [Input('transport-selector', 'value')]
+)
+
+def update_dropdown_styles(value):
+    bus_routes_style = {'display': 'block', 'color': '#000000'} if 'Bus' in value else {'display': 'block', 'color': '#000000'}
+    route_selector_style = {'display': 'block', 'color': '#000000'} if 'Subway' in value else {'display': 'block', 'color': '#000000'}
+    citibike_region_style = {'display': 'block', 'color': '#000000'} if 'Citibike' in value else {'display': 'block', 'color': '#000000'}
+    
+    return bus_routes_style, route_selector_style, citibike_region_style
+
+@app.callback(
+    Output('bus-routes-dropdown', 'options'),
+    [Input('boroughs_chosen', 'value')]
+)
+
+def update_bus_route_options(boroughs):
+    options = []
+    if boroughs is not None:
+        for borough in boroughs:
+            if borough == 'Bronx':
+                options.extend([{'label': str(route_id), 'value': route_id} for route_id in bus_bronx_id])
+            elif borough == 'Brooklyn':
+                options.extend([{'label': str(route_id), 'value': route_id} for route_id in bus_brooklyn_id])
+            elif borough == 'Manhattan':
+                options.extend([{'label': str(route_id), 'value': route_id} for route_id in bus_manhattan_id])
+            elif borough == 'Queens':
+                options.extend([{'label': str(route_id), 'value': route_id} for route_id in bus_queens_id])
+            elif borough == 'Staten_Island':
+                options.extend([{'label': str(route_id), 'value': route_id} for route_id in bus_staten_island_id])
+    return options
+
+def update_subway_map(gdf, feeds):
+    fig = go.Figure()
+
+    all_route_ids = gdf['route_id'].unique()
+
+    for route_id in all_route_ids:
+        route = gdf[gdf['route_id'] == route_id]
+        max_sequence = route['stop_sequence'].max()
+        max_sequence_index = route[route['stop_sequence'] == max_sequence].index[0]
+        longest_sequence = route.loc[max_sequence_index - max_sequence + 1:max_sequence_index]
+        
+        entity_dict = {}
+        for feed in feeds:
+            if feed['route'] == route_id:
+                arrival_time_str = feed['arrival_time']  
+                arrival_time_datetime = datetime.strptime(arrival_time_str, "%Y-%m-%d %H:%M:%S")  
+
+                departure_time_str = feed['departure_time']  
+                departure_time_datetime = datetime.strptime(departure_time_str, "%Y-%m-%d %H:%M:%S") 
+
+                entity_dict[str(feed['stop_id'])] = (arrival_time_datetime, departure_time_datetime)  
+
+        fig.add_trace(go.Scattermapbox(
+            lon=longest_sequence.geometry.x,
+            lat=longest_sequence.geometry.y,
+            mode='markers+lines',
+            marker=dict(symbol='circle', color="white", size=4),
+            text=longest_sequence.apply(lambda x: f"Route: {x['route_id']} <br> Stop Name: {x['stop_name']} <br> Arrival Time: {entity_dict.get(str(x['stop_id']), ('N/A', 'N/A'))[0]} <br> Departure Time: {entity_dict.get(str(x['stop_id']), ('N/A', 'N/A'))[1]}", axis=1),
+            hoverinfo='text',
+            line=dict(width=3, color=longest_sequence['color'].iloc[0])
+        ))
+        
+        
+    return fig
+
+def update_citibike_map():
+    citibike_gdf = citibike_station_data()
+
+    citibike_gdf = citibike_gdf[citibike_gdf['station_status'] == 'active']
+
+    citibike_gdf['last_reported'] = citibike_gdf['last_reported'].apply(lambda x: datetime.fromtimestamp(int(x)))
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scattermapbox(
+        lon=citibike_gdf.geometry.x,
+        lat=citibike_gdf.geometry.y,
+        mode='markers',
+        marker=dict(
+            size=4,
+            color='blue'
+        ),
+        text=citibike_gdf.apply(lambda x: f"Name: {x['name']} <br> Available Docks: {x['num_docks_available']} <br> Available eBikes: {x['num_ebikes_available']} <br> Available Bikes: {x['num_bikes_available']} <br> Last Reported: {x['last_reported']}", axis=1),
+        hoverinfo='text'
+    ))
+
+    return fig
+
+@app.callback(
+    [Output('map', 'figure'),
+     Output('real-time-data', 'children')],
+    [Input('transport-selector', 'value'),
+     Input('route-selector', 'value'),
+     Input('boroughs_chosen', 'value'),
+     Input('bus-routes-dropdown', 'value'),
+     Input('interval-component', 'n_intervals')]
+)
+
+def update_map_and_real_time_data(transportation, subway_routes, boroughs, bus_routes, n):
+    fig = go.Figure()
+    subway_schedule = None
+    bus_schedule = None
+    schedule_json = None
+    
+    if 'Subway' in transportation and subway_routes is not None:
+        subway_schedule = export_subway_schedule(subway_API_KEY)
+        subway_gdf = dataframes['subway'][dataframes['subway']['route_id'].isin(subway_routes)]
+        subway_fig = update_subway_map(subway_gdf, subway_schedule)
+        for trace in subway_fig.data:
+            fig.add_trace(trace)
+        
+    if 'Bus' in transportation and boroughs is not None and bus_routes is not None:
+        bus_schedule = export_bus_schedule(bus_API_KEY)
+        for borough in boroughs:
+            bus_gdf = dataframes[f'bus_{borough.lower()}'][dataframes[f'bus_{borough.lower()}']['route_id'].isin(bus_routes)]
+            bus_fig = update_subway_map(bus_gdf, bus_schedule)
+            for trace in bus_fig.data:
+                fig.add_trace(trace)
+    
+    if 'Citibike' in transportation:
+        citibike_fig = update_citibike_map()
+        for trace in citibike_fig.data:
+            fig.add_trace(trace)
+
+    fig.update_layout(
+        mapbox = {
+            'center': {"lat": 40.8, "lon": -74},
+            'style': "carto-darkmatter",
+            'zoom': 10
+        },
+        height = 1400,
+        paper_bgcolor = "#e4ebf5",
+        legend = dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="left",
+            x=0,
+            font=dict(
+                size=20,
+                color="#0e1012"
+            ),
+            bgcolor= "#e4ebf5",
+            bordercolor="#262729",
+            borderwidth=0
+        )    
+    )
+    
+    if subway_schedule :
+        schedule_json = json.dumps(subway_schedule)
+    if bus_schedule:
+        schedule_json = json.dumps(bus_schedule)
+        
+    return fig, schedule_json
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
